0ef2008f92a875b4d33629360c210811391e483c
Heberth Valverde <hgvaldela@gmail.com>
2016-07-06 23:06:50 -0600

    Correcion Programa C

--- sum.c
+++ sum.c
@@ -3,19 +3,17 @@
 int function(int a, int b, int c, int d, int e, int f, int g)
 {
 	int result=0;
-	result=a+b+c+d-e-f;
-	if(g>4){
-		printf("El numero es mayor a 4.\n");
+	if  (g>4) {
+        result = a + b + c + d - e - f;
+	} else {
+	    result = a + b + c + d + e - f;
 	}
-	else{
-		printf("El numero NO es mayor a 4.\n");
-	}
-	printf("El resultado de la operacion es %4d .\n",result);
 	return result;
 }
 
 int main()
 {
-	function(1,2,3,4,5,6,7);
-	 return 0;
+	int res = function(1,2,3,4,5,6,7);
+	printf("El resultado de la operacion es %4d .\n",res);
+    return 0;
 }
